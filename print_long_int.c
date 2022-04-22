455254c1f174caae9ceb174bab1c67258d812e1d
Peter_Bass <peter.oluwamayowa.akojede@gmail.com>
2022-04-22 14:54:24 +0100

    printf contribution

--- print_long_int.c
+++ print_long_int.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
+ * Author: Akojede peter and Makinde Samuel
  */
 int prinlint(va_list arguments, char *buf, unsigned int ibuf)
 {
