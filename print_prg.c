67265ab4b67b647f4a1e317a7c75a3c9a857036b
Peter_Bass <peter.oluwamayowa.akojede@gmail.com>
2022-04-22 15:18:52 +0100

    printf contribution

--- print_prg.c
+++ print_prg.c
@@ -7,6 +7,7 @@
  * @buf: buffer pointer
  * @i: index for buffer pointer
  * Return: On success 1.
+ * Author: Akojede Peter and Makinde Samuel
  */
 int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i)
 {
